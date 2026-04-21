f99fec751f81f0c3741ee0cb144b1244a51361bd
Maksymilian Golab <s33192@pjwstk.edu.pl>
2026-04-21 15:38:09 +0200

    Pokoje i rezerwacje

---
+++ Program/Models/Reservation.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Program.Models;
+
+public class Reservation : IValidatableObject
+{
+    public int Id { get; set; }
+
+    [Range(1, int.MaxValue, ErrorMessage = "RoomId must be greater than zero.")]
+    public int RoomId { get; set; }
+
+    [Required]
+    public string? OrganizerName { get; set; }
+
+    [Required]
+    public string? Topic { get; set; }
+
+    [Required]
+    public DateOnly? Date { get; set; }
+
+    [Required]
+    public TimeOnly? StartTime { get; set; }
+
+    [Required]
+    public TimeOnly? EndTime { get; set; }
+
+    [Required]
+    public string? Status { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (StartTime.HasValue && EndTime.HasValue && EndTime <= StartTime)
+        {
+            yield return new ValidationResult(
+                "EndTime must be later than StartTime.",
+                new[] { nameof(EndTime), nameof(StartTime) });
+        }
+    }
+}
+
